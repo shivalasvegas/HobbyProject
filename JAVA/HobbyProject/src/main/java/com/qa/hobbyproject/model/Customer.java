d45aa36ffc2ac44bc3be31ac8da39a7115d99398
shivalasvegas <shiva.king@academytrainee.com>
2020-08-02 21:04:00 +0100

    HP-28 :: SK - Customer.java: variables/getters/setters added

--- JAVA/HobbyProject/src/main/java/com/qa/hobbyproject/model/Customer.java
+++ JAVA/HobbyProject/src/main/java/com/qa/hobbyproject/model/Customer.java
@@ -1,5 +1,66 @@
 package com.qa.hobbyproject.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
 
+	@Id
+	@GeneratedValue
+	@Column(name="customer_id")
+	private int customerId;
+	@Column(length=50, name="customer_name")
+	private String customerName;
+	@Column(length=100, name="customer_address")
+	private String customerAddress;
+	@Column(length=20, name="customer_phone")
+	private String customerPhone;
+	@Column(length=20, name="customer_email")
+	private String customerEmail;
+	@Column(length=15, name="customer_password")
+	private String customerPassword;
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+	public String getCustomerPhone() {
+		return customerPhone;
+	}
+	public void setCustomerPhone(String customerPhone) {
+		this.customerPhone = customerPhone;
+	}
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+	public String getCustomerPassword() {
+		return customerPassword;
+	}
+	public void setCustomerPassword(String customerPassword) {
+		this.customerPassword = customerPassword;
+	}
+	
+	
+	
+	
 }
