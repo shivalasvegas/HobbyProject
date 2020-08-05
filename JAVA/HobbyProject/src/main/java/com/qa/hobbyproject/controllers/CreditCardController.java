1ce7cae2eaaa9e3c98fd31ad6af573dd1b16da50
shivalasvegas <shiva.king@academytrainee.com>
2020-08-05 04:48:40 +0100

    HP-30 :: SK - CreditCardController.java: added crud methods

--- JAVA/HobbyProject/src/main/java/com/qa/hobbyproject/controllers/CreditCardController.java
+++ JAVA/HobbyProject/src/main/java/com/qa/hobbyproject/controllers/CreditCardController.java
@@ -1,5 +1,56 @@
 package com.qa.hobbyproject.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.hobbyproject.exceptions.CardNotFoundException;
+import com.qa.hobbyproject.model.CreditCard;
+import com.qa.hobbyproject.services.CreditCardService;
+
+@RestController
 public class CreditCardController {
 
+	@Autowired
+	CreditCardService service;
+
+	@PostMapping("/createcard")
+	public String createCardRecord(@RequestBody CreditCard card) {
+		String message = this.service.createCard(card);
+
+		return message;
+	}
+
+	@GetMapping("/readallcards")
+	public List<CreditCard> readAllCardRecords() {
+		List<CreditCard> record = service.readAllCards();
+
+		return record;
+	}
+
+	// @PutMapping("/updatecard/{id}")
+
+	@DeleteMapping("/deletecard/{id}")
+
+	public String deleteCard(@PathVariable int id) throws CardNotFoundException {
+		boolean deleted = this.service.deleteCard(id);
+
+		String message;
+		try {
+			if (deleted)
+				message = "Card deleted";
+			else
+				message = "Id does not exist";
+		} catch (CardNotFoundException cardException) {
+			message = "Please enter another id";
+		}
+
+		return message;
+	}
 }
