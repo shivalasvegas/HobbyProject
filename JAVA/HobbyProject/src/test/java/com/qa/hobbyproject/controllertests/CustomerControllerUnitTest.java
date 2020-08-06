7c8c74a5dab31c16e14e0c8fb67b9b5c725db823
shivalasvegas <shiva.king@academytrainee.com>
2020-08-06 15:14:47 +0100

    HP-46 :: SK - updated pom.xml for mockito; customerControllerUnitTests: create/readall/delete

--- JAVA/HobbyProject/src/test/java/com/qa/hobbyproject/controllertests/CustomerControllerUnitTest.java
+++ JAVA/HobbyProject/src/test/java/com/qa/hobbyproject/controllertests/CustomerControllerUnitTest.java
@@ -4,13 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.hobbyproject.model.Customer;
 import com.qa.hobbyproject.repositories.CustomerRepository;
@@ -30,7 +38,7 @@ public class CustomerControllerUnitTest {
 	private ObjectMapper mapper = new ObjectMapper();
 	
 	private Customer testCustomer;
-	private Customer newTestCustomer;
+	//private Customer newTestCustomer;
 	private Customer testCustomerWithID;
 	private int id;
 	private List<Customer> listCustomer = new ArrayList<>();
@@ -47,5 +55,76 @@ public class CustomerControllerUnitTest {
 		this.listCustomer.add(testCustomerWithID);
 	}
 
+	@Test
+	public void testCreate() throws JsonProcessingException, Exception{
+		MockHttpServletRequestBuilder mockRequest = 
+				MockMvcRequestBuilders.request(HttpMethod.POST, "/customer/create");
+		
+		mockRequest.contentType(MediaType.APPLICATION_JSON)
+			.content(this.mapper.writeValueAsString(testCustomerWithID))
+			.accept(MediaType.APPLICATION_JSON);
+	
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isCreated();
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(testCustomerWithID));
+		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
+	}
+	
+//	@Test
+//	public void testRead() throws JsonProcessingException, Exception {
+//		MockHttpServletRequestBuilder mockRequest = 
+//				MockMvcRequestBuilders.request(HttpMethod.GET, "/customer/read/1");
+//		
+//		mockRequest.contentType(MediaType.APPLICATION_JSON)
+//			.content(this.mapper.writeValueAsString(testCustomerWithID))
+//			.accept(MediaType.APPLICATION_JSON);
+//	
+//		ResultMatcher matchStatus = MockMvcResultMatchers.status().isOk();
+//		ResultMatcher matchContent = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(testCustomerWithID));
+//		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
+//	}
+	
+	@Test
+	public void testReadAll() throws JsonProcessingException, Exception {
+		MockHttpServletRequestBuilder mockRequest = 
+				MockMvcRequestBuilders.request(HttpMethod.GET, "/customer/readAll");
+		
+		mockRequest.contentType(MediaType.APPLICATION_JSON)
+			.content(this.mapper.writeValueAsString(listCustomer))
+			.accept(MediaType.APPLICATION_JSON);
+	
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isOk();
+		ResultMatcher matchContent = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(listCustomer));
+		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
+	}
+	
+//	@Test
+//	public void testUpdate() throws JsonProcessingException, Exception {
+//		this.newTestCustomer = new Customer("Doctor Who", "The Tardis", "000004442227456", "thedoctor@universe.com", "boring");
+//		this.newTestCustomer.setId(testCustomerWithID.getId());
+//	MockHttpServletRequestBuilder mockRequest = 
+//	MockMvcRequestBuilders.request(HttpMethod.PUT, "/card/update/" + testCustomerWithID.getId());
+//		
+//	mockRequest.contentType(MediaType.APPLICATION_JSON)
+//	.content(this.mapper.writeValueAsString(newTestCustomer))
+//	.accept(MediaType.APPLICATION_JSON);
+//
+//	ResultMatcher matchStatus = MockMvcResultMatchers.status().isAccepted();
+//	ResultMatcher matchContent = MockMvcResultMatchers.content().json(this.mapper.writeValueAsString(newTestCustomer));
+//	this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchContent);
+//	}
+	
+	@Test
+	public void testDelete() throws JsonProcessingException, Exception {
+		MockHttpServletRequestBuilder mockRequest = 
+				MockMvcRequestBuilders.request(HttpMethod.DELETE, "/customer/delete/1" + testCustomerWithID.getId());
+		
+		mockRequest.contentType(MediaType.APPLICATION_JSON)
+			.content(this.mapper.writeValueAsString(testCustomerWithID))
+			.accept(MediaType.APPLICATION_JSON);
+	
+		ResultMatcher matchStatus = MockMvcResultMatchers.status().isNoContent();
+		this.mock.perform(mockRequest).andExpect(matchStatus);
+	}
+	
 	
 }
