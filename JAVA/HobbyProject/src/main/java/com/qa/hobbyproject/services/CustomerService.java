8e4ce30fde2d0b616697500fab0abdd3aa4c3f34
shivalasvegas <shiva.king@academytrainee.com>
2020-08-09 19:34:52 +0100

    HP-46 :: SK - CustomerService.java: added Logger re-wrote updateCutomer method

--- JAVA/HobbyProject/src/main/java/com/qa/hobbyproject/services/CustomerService.java
+++ JAVA/HobbyProject/src/main/java/com/qa/hobbyproject/services/CustomerService.java
@@ -1,6 +1,7 @@
 package com.qa.hobbyproject.services;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,56 +12,55 @@ import com.qa.hobbyproject.repositories.CustomerRepository;
 
 @Service
 public class CustomerService {
+
+	private final static Logger LOGGER = Logger.getLogger(Logger.class.getName());
 	
 	@Autowired
 	CustomerRepository customerRepo;
-	
-	//Create
+
+	// Create
 	public String createCustomer(Customer customer) {
 		this.customerRepo.save(customer);
 		return "Customer record created";
 	}
-	
-	//Read
+
+	// Read
 	public Customer readCustomer(int id) {
 		Customer customer = this.customerRepo.findById(id).orElseThrow(IdNotFoundException::new);
+		LOGGER.info("Read Customer with id: " + customer.getCustomerId()); 
 		return customer;
 	}
-	
-	
 
 	public List<Customer> readAllCustomers() {
 		List<Customer> customerRecords = this.customerRepo.findAll();
+		LOGGER.info("Read all customer records");
 		return customerRecords;
-	
+
 	}
-	
 
-	//Update
-	public Customer updateCustomer(Customer newCustomer, int id) {
-	Customer updateCustomer = readCustomer(id);
+	// Update
+	public Customer updateCustomer(Customer updatedCustomer, int id) {
+		Customer existingCustomer = this.customerRepo.findById(id).orElseThrow(IdNotFoundException::new);
 
-	updateCustomer.setCustomerName(newCustomer.getCustomerName());
-	updateCustomer.setCustomerAddress(newCustomer.getCustomerAddress());
-	updateCustomer.setCustomerPhone(newCustomer.getCustomerPhone());
-	updateCustomer.setCustomerEmail(newCustomer.getCustomerEmail());
-	updateCustomer.setCustomerPassword(newCustomer.getCustomerPassword());
+		existingCustomer.setCustomerName(updatedCustomer.getCustomerName());
+		existingCustomer.setCustomerAddress(updatedCustomer.getCustomerAddress());
+		existingCustomer.setCustomerPhone(updatedCustomer.getCustomerPhone());
+		existingCustomer.setCustomerEmail(updatedCustomer.getCustomerEmail());
+		existingCustomer.setCustomerPassword(updatedCustomer.getCustomerPassword());
+		LOGGER.info("Updated customer with id: " + existingCustomer.getCustomerId());
+		Customer saved = this.customerRepo.save(existingCustomer);
+		return saved;
+	}
 
-	Customer saved = this.customerRepo.save(updateCustomer);
-	return saved;
-}
-
-	
-	//Delete
+	// Delete
 	public boolean deleteCustomer(int id) {
+		Customer customer = this.customerRepo.findById(id).orElseThrow(IdNotFoundException::new);
+		LOGGER.info("Deleting customer with id: " + customer.getCustomerId());
+		
 		this.customerRepo.deleteById(id);
 		boolean deleted = !this.customerRepo.existsById(id);
+		LOGGER.info("Customer deleted");
 		return deleted;
 	}
-
-	
-	
-	
-	
 
 }
