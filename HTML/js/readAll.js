7cc2ed8ae7e802d1af2f3de68559912b48a92637
shivalasvegas <shiva.king@academytrainee.com>
2020-08-11 17:07:53 +0100

    HP-33 :: SK - adminportal.html: Edited for create/read; readAll.js: created

---
+++ HTML/js/readAll.js
@@ -0,0 +1,81 @@
+
+function readAll(){
+fetch('http://localhost:8003/readall')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+        drawTable(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+
+  }
+
+  function drawTable(jsondata){
+    let mountains = jsondata;
+      
+      function generateTableHead(table, data) {
+        let thead = table.createTHead();
+        let row = thead.insertRow();
+        for (let key of data) {
+          let th = document.createElement("th");
+          let text = document.createTextNode(key);
+          th.appendChild(text);
+          // <tr>
+          // <th>what ever the key is </th>
+          // </tr>
+          row.appendChild(th);
+        }
+        let a = document.createElement("th")
+        let a1 = document.createElement("th")
+        let b =document.createTextNode("Delete")
+        let c= document.createTextNode("Update")
+        a1.appendChild(b)
+        a.appendChild(c)
+        row.appendChild(a)
+        row.appendChild(a1)
+      }
+      //console.log("My id is :",element.salesId)
+          function generateTable(table, data) {
+            for (let element of data) {
+              let row = table.insertRow();
+              for (key in element) {
+                //console.log(element.salesId)
+                
+                let cell = row.insertCell();
+                let text = document.createTextNode(element[key]);
+                cell.appendChild(text);
+              }
+              let operation =row.insertCell()
+              let aTag = document.createElement("a");
+              aTag.href = `http://localhost:8003/update/${element.salesId}`
+              aTag.innerHTML = "Update"
+              operation.appendChild(aTag)
+              let operation1 =row.insertCell()
+              let aTagupdate = document.createElement("a");
+              aTagupdate.href = `http://localhost:8003/delete/${element.salesId}`
+              aTagupdate.innerHTML = "Delete"
+              operation1.appendChild(aTagupdate)
+            }
+          }
+         
+          let table = document.querySelector("table");
+          let data = Object.keys(mountains[0]);
+          generateTableHead(table, data);
+          generateTable(table, mountains);
+        }
+    
+
+
