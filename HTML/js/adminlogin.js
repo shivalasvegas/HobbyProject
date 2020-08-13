c9b8b8a6871559fe1260c7df8cb41e6e673a071f
shivalasvegas <shiva.king@academytrainee.com>
2020-08-13 21:25:46 +0100

    HP-49 :: SK - adminlogin: Updated with fetch link to database for validation

--- HTML/js/adminlogin.js
+++ HTML/js/adminlogin.js
@@ -1,16 +1,23 @@
 
 function validate(form){
 
-if (form.userid.value == "admin@admin.com" && form.userpwd.value == "admin"){
-alert ("Login successful");
-//window.location="adminportal.html";
+    const url = `https://localhost:8003/readalladmins`;
+    async function getData(){
+    
+        const response = await fetch(url);
+        const data = await response.json();
 
-window.open("adminportal.html"); // Redirecting to other page.
-}
-else{
-
-alert("Wrong username or password");
-// Disabling fields after 3 attempts.
-
-}
+        for (let i = 0; i <=2; i++) {
+                console.log("Name: " + data.results[i].adminName);
+                console.log("Username: " + data.results[i].adminEmail);
+                console.log("Password: " + data.results[i].adminPassword);
+            if (form.userid.value == data.results[i].adminEmail && form.userpwd.value == data.results[i].adminPassword){
+                alert ("Login successful");  
+            window.open("adminportal.html"); // Redirecting to other page.
+            }
+            else{
+                alert("Wrong username or password");
+            }
+        }
+    }
 }
