5556be3a4bfe72864d34138b853690ea13b5716c
shivalasvegas <shiva.king@academytrainee.com>
2020-08-16 20:51:58 +0100

    HP-49 :: SK - Adminportal: changed styling; added update admin details

--- HTML/js/adminlogin.js
+++ HTML/js/adminlogin.js
@@ -1,21 +1,38 @@
 
-function validate(form){
+let adminLoggedIn = false;
 
+//function validate(form){
+    console.log("Inside admin form");
     const url = `https://localhost:8003/readalladmins`;
-    async function getData(){
-    
+    async function getData(form){
+        console.log("Inside getData");
         const response = await fetch(url);
         const data = await response.json();
+        console.log(data);
 
         for (let i = 0; i <=2; i++) {
                 
             if (form.userid.value == data.results[i].adminEmail && form.userpwd.value == data.results[i].adminPassword){
+                adminLoggedIn = true;
                 alert ("Login successful");  
-                window.open("adminportal.html"); 
+                window.open("adminportal.html");
             }
             else{
                 alert("Wrong username or password");
+
             }
         }
+
+    }
+    
+//}
+
+let adminIn = function(){
+    if (!adminLoggedIn){
+        return false;
+    }
+    else{
+        return true;
     }
 }
+
